2fdd774e3f420785576a34898d04fddd120833e5
Rishidt <rpdiwte@gmail.com>
2023-04-23 11:35:52 +0530

    Textbox control : used focus method on two textboxes at a time.

---
+++ ASPNETLearnings/TextboxControl.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETLearnings
+{
+    public partial class TextboxControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Focus();
+            TextBox2.Focus(); //this will be focused 
+
+
+        }
+    }
+}
